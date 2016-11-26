dc1786d3cd02146cb5c26db4274a650c135af69e
Makagucci <makagucci@gmx.com>
2016-11-26 08:34:38 +0000

    0.1

---
+++ default-values.cc
@@ -0,0 +1,18 @@
+// default values in functions
+#include <iostream>
+using namespace std;
+
+int divide (int a, int b=2)
+{
+  int r;
+  r=a/b;
+  return (r);
+}
+
+int main()
+{  
+  cout << divide (12) << endl;
+  cout << divide (20,4) << endl;
+  return 0;
+}
+
